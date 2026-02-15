602c26f3722101863bec8a7e67c5978bfd42d82f
hazemahmed-art <hazemahmedmoh97@gmail.com>
2026-02-15 21:07:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ with col3:
     st.image("logo.png", width=220)
 
 # ================== قراءة ملف Excel ==================
-file_path = "v5.xlsx"
+file_path = "v6.xlsx"
 try:
     df = pd.read_excel(file_path, header=None)
 except Exception as e:
@@ -174,6 +174,7 @@ if selected_family and selected_family != "- اختر عائلة -":
 
 else:
     st.info("الرجاء اختيار العائلة لبدء العرض.")
+
 
 
 
